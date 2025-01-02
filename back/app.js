576927d1b1621e4dcae4291c20565ee7bc2e2030
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-02 19:23:21 +0100

    amelioration file server.js

--- back/app.js
+++ back/app.js
@@ -4,10 +4,21 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log('requete via console.log');
+    next();
+})
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
 })
 
 app.use((req, res, next) => {
     res.json({message: 'requete reçue !'});
+    next();
+});
+
+app.use((req, res, ) => {
+    console.log('middleware fonctionnel');
 });
 
 module.exports = app;
