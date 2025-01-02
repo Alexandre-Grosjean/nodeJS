62b03e13eeede88d54ef930ab08eb7966cf09fa3
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-02 19:08:23 +0100

    ajout package express

--- back/app.js
+++ back/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('requete via console.log');
+})
+
+app.use((req, res, next) => {
     res.json({message: 'requete reçue !'});
 });
 
