a34b71ede5228a5cf13b81ebebf30ff2b1554b81
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-02 19:00:43 +0100

    app.js file create with localserver 3000

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.end('Voila la reponse du serveur !');
-});
+const app = require('./app');
+
+app.set('port', process.env.POR || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
