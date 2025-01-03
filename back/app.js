8b94f130496c38276c09572abd3c12ec8f828ba2
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-03 17:04:42 +0100

    connexion mongo atlas fixed

--- back/app.js
+++ back/app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 
-mongoose.connect('mongodb+srv://AlexandreGrosjean:O1bmFvRKOQRmijqH@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://AlexandreGrosjean:a7r68SCTrIsYltZS@cluster0.b2khq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     )
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
